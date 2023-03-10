fb76df3f6bf253b7aa6ccea684d1974a8569f1ce
▄︻̷̿┻̿═━一ZombieStriker <yse33@abv.bg>
2023-03-10 23:42:27 +0200

    Added working extension for multiple account on one domain and password generator script

--- extension/content-script.js
+++ extension/content-script.js
@@ -1,50 +1,98 @@
 const api = "http://localhost:3000/get";
 const get = "get";
 
-let data = {};
+let emailField;
+let userInfo = [];
 let input = document.getElementsByTagName('input');
 let counter = 0;
 
-const passwordField = document.querySelector('input[type="password"]');
-passwordField.addEventListener('click', () => {
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET', api);
-    xhr.onload = () => {
-        if (xhr.status === 200) {
-            data = JSON.parse(xhr.responseText);
-        } else {
-            console.log("ERROR");
-        }
-    };  
-    xhr.send();
-    let suggestions = [];
-    for (let i = 0; i < data.length; i++) {
-        suggestions.push(data[i]);
+const nameOrEmailFields = document.querySelectorAll('input[type="email"], input[type="text"], input[name*="name"], input[name*="email"], input[name="log"], input[name="email-form"], input[name="username"]');
+for (let field of nameOrEmailFields) {
+    if(field.name) {
+        emailField = field;
+        break;
     }
+}
 
-    showPasswordSuggestions(passwordField, suggestions);
-    
-});
+const passwordField = document.querySelector('input[type="password"]');
+if (passwordField != null) {
+    passwordField.addEventListener('click', () => {
+        const xhr = new XMLHttpRequest();
+        xhr.open('GET', api);
+        xhr.onload = () => {
+            if (xhr.status === 200) {
+                userInfo = JSON.parse(xhr.responseText);
+            } else {
+                console.log("ERROR");
+            }
+        };  
+        xhr.send();
 
-function showPasswordSuggestions(input, suggestions) {
+        showPasswordSuggestions(emailField, passwordField, userInfo);
+        
+    });
+
+    emailField.addEventListener('click', () => {
+        const xhr = new XMLHttpRequest();
+        xhr.open('GET', api);
+        xhr.onload = () => {
+            if (xhr.status === 200) {
+                userInfo = JSON.parse(xhr.responseText);
+            } else {
+                console.log("ERROR");
+            }
+        };  
+        xhr.send();
+
+        showEmailSuggestions(emailField, passwordField, userInfo);
+        
+    });
+}
+
+function showEmailSuggestions(emailField, passwordField, userInfo) {
     if (counter < 2) {
         counter++;
         let suggestionList = document.createElement("ul");
         suggestionList.style.cssText = "position:absolute;z-index:9999;padding:0;margin:0;background-color:#fff;border:1px solid #ccc;list-style:none;max-height:200px;overflow-y:auto;";
-        suggestions.forEach(function(password) {
+        userInfo.forEach(function(credentials) {
             let suggestionItem = document.createElement("li");
             suggestionItem.style.cssText = "padding:5px;cursor:pointer;";
-            suggestionItem.textContent = password.Username + " (" + password.Id + ")";
+            suggestionItem.textContent = credentials.EmailInDomain;
             suggestionItem.addEventListener("click", function() {
-                input.value = password.Id;
+                emailField.value = credentials.EmailInDomain;
+                passwordField.value = credentials.Password;
                 suggestionList.parentNode.removeChild(suggestionList);
                 counter--;
             });
-        suggestionList.appendChild(suggestionItem);
+            suggestionList.appendChild(suggestionItem);
         });
 
-        input.parentNode.appendChild(suggestionList);
-        suggestionList.style.top = input.offsetTop + input.offsetHeight + "px";
-        suggestionList.style.left = input.offsetLeft + "px";
+        emailField.parentNode.appendChild(suggestionList);
+        suggestionList.style.top = emailField.offsetTop + emailField.offsetHeight + "px";
+        suggestionList.style.left = emailField.offsetLeft + "px";
+    }
+}
+
+function showPasswordSuggestions(emailField, passwordField, userInfo) {
+    if (counter < 2) {
+        counter++;
+        let suggestionList = document.createElement("ul");
+        suggestionList.style.cssText = "position:absolute;z-index:9999;padding:0;margin:0;background-color:#fff;border:1px solid #ccc;list-style:none;max-height:200px;overflow-y:auto;";
+        userInfo.forEach(function(credentials) {
+            let suggestionItem = document.createElement("li");
+            suggestionItem.style.cssText = "padding:5px;cursor:pointer;";
+            suggestionItem.textContent = "Use password for ".concat(credentials.EmailInDomain);
+            suggestionItem.addEventListener("click", function() {
+                emailField.value = credentials.EmailInDomain;
+                passwordField.value = credentials.Password;
+                suggestionList.parentNode.removeChild(suggestionList);
+                counter--;
+            });
+            suggestionList.appendChild(suggestionItem);
+        });
+
+        passwordField.parentNode.appendChild(suggestionList);
+        suggestionList.style.top = passwordField.offsetTop + passwordField.offsetHeight + "px";
+        suggestionList.style.left = passwordField.offsetLeft + "px";
     }
 }
