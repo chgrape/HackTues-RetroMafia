dceedd996793054e4fca20397cf39598fb9c5116
burger <krisugergaming@gmail.com>
2023-03-10 16:40:25 +0200

    Added endpoint

--- frontend/src/components/PassPage.js
+++ frontend/src/components/PassPage.js
@@ -1,13 +1,16 @@
 import { Typography } from '@mui/material'
 import React, { useEffect, useState } from 'react'
 
-const url = 'https://jsonplaceholder.typicode.com/posts'
-
 function PassPage() {
     const [passwords, setPasswords] = useState([]);
 
     const fetchPasswords = async () =>{
-        const response = await fetch(url);
+        const response = await fetch('https://91c6-151-251-243-143.eu.ngrok.io/user',{
+            headers:new Headers({
+                "ngrok-skip-browser-warning": true
+                
+            }),
+        });
         const data = await response.json();
         console.log(data);
         setPasswords(data);
@@ -21,7 +24,7 @@ function PassPage() {
         <>
             <Typography sx={{display:"flex", justifyContent:"center", fontSize: 20, p:5}}>Password page</Typography>
             
-            {passwords.map((pass)=>{return <Typography key={pass.id} variant="h5" sx={{display:"flex", justifyContent:"center"}} >Password for {pass.title} - {pass.userId}</Typography>})}
+            {passwords.map((pass)=>{return <Typography key={pass.id} variant="h5" sx={{display:"flex", justifyContent:"center"}} >Password for {pass.Id} - {pass.Username}</Typography>})}
         </>
     )
 }
