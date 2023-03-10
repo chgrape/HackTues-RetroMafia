6172521b3ca71e90887d363de1903ba1804936fe
▄︻̷̿┻̿═━一ZombieStriker <yse33@abv.bg>
2023-03-10 16:04:45 +0200

    Extension base features

--- extension/content-script.js
+++ extension/content-script.js
@@ -1,16 +1,50 @@
-let input = document.getElementsByTagName('input')
-if(input){
-    console.log(input)
-    for (el of input){
-        console.log(el)
-        let type = el.getAttribute("type");
-        let id = el.getAttribute("id")
-        if (type.toLowerCase().includes("password") || id.toLowerCase().includes("password")){
-            console.log("is password")
-            el.value = "ihateni"
+const api = "http://localhost:3000/get";
+const get = "get";
+
+let data = {};
+let input = document.getElementsByTagName('input');
+let counter = 0;
+
+const passwordField = document.querySelector('input[type="password"]');
+passwordField.addEventListener('click', () => {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', api);
+    xhr.onload = () => {
+        if (xhr.status === 200) {
+            data = JSON.parse(xhr.responseText);
+        } else {
+            console.log("ERROR");
         }
-        console.log("Input form")
+    };  
+    xhr.send();
+    let suggestions = [];
+    for (let i = 0; i < data.length; i++) {
+        suggestions.push(data[i]);
     }
-}else{
-    console.log("no inputs")
+
+    showPasswordSuggestions(passwordField, suggestions);
+    
+});
+
+function showPasswordSuggestions(input, suggestions) {
+    if (counter < 2) {
+        counter++;
+        let suggestionList = document.createElement("ul");
+        suggestionList.style.cssText = "position:absolute;z-index:9999;padding:0;margin:0;background-color:#fff;border:1px solid #ccc;list-style:none;max-height:200px;overflow-y:auto;";
+        suggestions.forEach(function(password) {
+            let suggestionItem = document.createElement("li");
+            suggestionItem.style.cssText = "padding:5px;cursor:pointer;";
+            suggestionItem.textContent = password.Username + " (" + password.Id + ")";
+            suggestionItem.addEventListener("click", function() {
+                input.value = password.Id;
+                suggestionList.parentNode.removeChild(suggestionList);
+                counter--;
+            });
+        suggestionList.appendChild(suggestionItem);
+        });
+
+        input.parentNode.appendChild(suggestionList);
+        suggestionList.style.top = input.offsetTop + input.offsetHeight + "px";
+        suggestionList.style.left = input.offsetLeft + "px";
+    }
 }
